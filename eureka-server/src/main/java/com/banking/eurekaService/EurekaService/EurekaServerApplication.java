0f69e5f0b14f8f19e761ed2012a3b9ebb9f90d75
Akash Adak <akashadak00023@gmail.com>
2025-07-10 20:25:52 +0530

    eureka server

---
+++ eureka-server/src/main/java/com/banking/eurekaService/EurekaService/EurekaServerApplication.java
@@ -0,0 +1,9 @@
+package com.banking.eurekaService.EurekaService;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
