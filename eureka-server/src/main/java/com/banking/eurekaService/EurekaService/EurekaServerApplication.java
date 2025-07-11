27acc6ef4c2b727fc793e8d777f5aaf039b23d2e
Akash Adak <akashadak00023@gmail.com>
2025-07-11 19:37:13 +0530

    fixed eureka problempartially

--- eureka-server/src/main/java/com/banking/eurekaService/EurekaService/EurekaServerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.banking.eurekaService.EurekaService;
-
-@SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
-    }
-}
--- user-service/src/main/java/com/banking/user/KafkaProducer.java
+++ user-service/src/main/java/com/banking/user/KafkaProducer.java
@@ -1,3 +1,9 @@
+package com.banking.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
 @Service
 public class KafkaProducer {
     @Autowired
