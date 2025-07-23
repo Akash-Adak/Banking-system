14f371462970a884e90dd74fef8be6f6008f0312
Akash Adak <akashadak00023@gmail.com>
2025-07-23 23:03:36 +0530

    add all docker file

--- notification-server/src/main/java/com/banking/notification/model/AccountCreatedEvent.java
+++ notification-server/src/main/java/com/banking/notification/model/AccountCreatedEvent.java
@@ -9,4 +9,36 @@ public class AccountCreatedEvent {
     private String phone;
     private String accountNumber;
     private String body;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
--- notification-server/src/main/java/com/banking/notification/model/TransactionCompletedEvent.java
+++ notification-server/src/main/java/com/banking/notification/model/TransactionCompletedEvent.java
@@ -10,4 +10,44 @@ public class TransactionCompletedEvent {
     private String transactionId;
     private double amount;
     private String body;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
