328ce7470c385d45ca97f225e9513758af833cbd
Akash Adak <akashadak00023@gmail.com>
2025-11-25 23:08:55 +0530

    vite config chaned

--- banking-frontend/vite.config.js
+++ banking-frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react({
-    include: ['**/*.js', '**/*.jsx'] // Process both .js and .jsx files as JSX
-  })],
+   plugins: [react],
    server: {
      host: '0.0.0.0', 
     port: 9090,   // 👈 change this to whatever port you want
